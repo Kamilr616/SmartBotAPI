216f0e5fedd138a5ef5e8d2f232e1438f6a28b24
Kamil Rataj <105726592+Kamilr616@users.noreply.github.com>
2024-10-23 15:57:19 +0200

    Redirect controller

---
+++ src/api/SmartBotWebAPI/Controllers/RedirectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace SmartBotWebAPI.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RedirectController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Redirect("https://smartbotblazorapp.azurewebsites.net");
+        }
+
+    }
+}
