2ac3bfe10b37af625ea8a48b1dffb383e24347c6
Kamil Rataj <105726592+Kamilr616@users.noreply.github.com>
2024-11-25 13:52:16 +0100

    Layout update

--- src/server/SmartBotBlazorApp/Hubs/SignalHub.cs
+++ src/server/SmartBotBlazorApp/Hubs/SignalHub.cs
@@ -27,21 +27,22 @@ namespace SmartBotBlazorApp.Hubs
         }
 
         [HubMethodName("ReceiveRobotData")]
-        public async Task ReceiveRobotData(string user, float[] measurements, ushort[] rawMatrix, ushort avgDistance)
+        public async Task ReceiveRobotData(string user, double[] measurements, ushort[] rawMatrix, ushort avgDistance)
         {
             var base64Img = _imageProcessor.GenerateHeatmapBase64Image(rawMatrix);
+
+            RoundMeasurements(measurements, 2);
 
             await Clients.Others.SendAsync("ReceiveBase64Frame", $"SignalHub({user})", measurements, base64Img, avgDistance);
 
             await Clients.Others.SendAsync("ReceiveMatrix", $"SignalHub({user})", measurements, _imageProcessor.InterpolateData(rawMatrix, 32), avgDistance);
         }
-
-        [HubMethodName("ReceiveRobotDataA")]
-        public async Task ReceiveRobotDataA(string user, float[] measurements, ushort[] rawMatrix, ushort avgDistance)
+        private void RoundMeasurements(double[] measurementsArray, int precision = 2)
         {
-            var interpolatedMatrix = _imageProcessor.InterpolateData(rawMatrix, 32);
-
-            await Clients.Others.SendAsync("ReceiveMatrix", $"SignalHub({user})",  measurements, interpolatedMatrix, avgDistance);
+            for (int i = 0; i < measurementsArray.Length; i++)
+            {
+                measurementsArray[i] = Math.Round(measurementsArray[i], precision, MidpointRounding.ToEven);
+            }
         }
 
     }
