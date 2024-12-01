22db2ce25606c5b94dedb965fb7a35055d6f6d01
Matix351 <130908742+Matix351@users.noreply.github.com>
2024-12-01 22:30:47 +0100

    StyleINIMageViewCleanUpAndjoystickAdded

---
+++ src/server/SmartBotBlazorApp/Components/KeyboardInputHandler.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace SmartBotBlazorApp.Components
+{
+    public class keyboardInputHandler
+    {
+        public string keyName;
+        public int robotDir;
+        public bool validInput;
+
+        public keyboardInputHandler(KeyboardEventArgs e)
+        {
+
+            switch (e.Key)
+            {
+                case "ArrowUp":
+                    keyName = "Up Arrow";
+                    robotDir = 0;
+                    validInput = true;
+                    break;
+
+                case "ArrowDown":
+                    keyName = "Down Arrow";
+                    robotDir = 1;
+                    validInput = true;
+                    break;
+
+                case "ArrowLeft":
+                    keyName = "Left Arrow";
+                    robotDir = 2;
+                    validInput = true;
+                    break;
+
+                case "ArrowRight":
+                    keyName = "Right Arrow";
+                    robotDir = 3;
+                    validInput = true;
+                    break;
+
+                default:
+                    keyName = "Wrong Button";
+                    robotDir = -1;
+                    validInput = false;
+                    break;
+            }
+        }
+    }
+}
