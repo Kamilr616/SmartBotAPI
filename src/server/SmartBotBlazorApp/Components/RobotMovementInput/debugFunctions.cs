2c40e38d914163a804e3ed00c9ee65b07662d610
Matix351 <130908742+Matix351@users.noreply.github.com>
2024-12-04 11:14:27 +0100

    codeCleanup

---
+++ src/server/SmartBotBlazorApp/Components/RobotMovementInput/debugFunctions.cs
@@ -0,0 +1,36 @@
+﻿namespace SmartBotBlazorApp.Components.RobotMovementInput
+{
+    public class debugFunctions
+    {
+        static public ushort[] GenerateTestData(int resolution)
+        {
+            var random = new Random();
+            ushort[] data = new ushort[resolution * resolution];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (ushort)random.Next(0, 4000);
+            }
+
+            return data;
+        }
+
+        static public ushort[] GenerateTestDataAscend(int resolution)
+        {
+            ushort[] data = new ushort[resolution * resolution];
+
+            int index = 0;
+
+            for (int i = 0; i < resolution; i++)
+            {
+                for (int j = 0; j < resolution; j++)
+                {
+                    data[index] = (ushort)(i * 125 + j);
+                    index++;
+                }
+            }
+
+            return data;
+        }
+    }
+}
