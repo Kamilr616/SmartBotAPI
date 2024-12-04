54b8b55e1e03890788487ba8255a3e06f96bcfb1
Matix351 <130908742+Matix351@users.noreply.github.com>
2024-12-05 00:00:29 +0100

    AuthorizationAdded

--- src/server/SmartBotBlazorApp.Client/Program.cs
+++ src/server/SmartBotBlazorApp.Client/Program.cs
@@ -2,7 +2,10 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartBotBlazorApp.Client;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
---
+++ src/server/SmartBotBlazorApp/Data/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartBotBlazorApp.Data
+{
+    public class SeedData
+    {
+
+        public static async Task Initialize(
+            IServiceProvider serviceProvider,
+            string password
+            )
+        {
+            
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                var manager = await EnsureUser(serviceProvider, "admin@admin.pl", password);
+
+            }
+        }
+        private static async Task<string> EnsureUser(
+            IServiceProvider serviceProvider,
+            string userName, string initPw)
+        {
+            var _userManager = serviceProvider.GetService<UserManager<ApplicationUser>>();
+
+            var user = await _userManager.FindByNameAsync(userName);
+
+            if(user == null)
+            {
+                user = new ApplicationUser
+                {
+                    UserName = userName,
+                    Email = userName,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, initPw);
+
+            }
+
+            if (user == null)
+                throw new Exception("User did not get created. Password Problem");
+
+            return user.Id;
+        }
+            
+
+
+    }
+
+
+}
