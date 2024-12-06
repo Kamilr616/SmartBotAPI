52e168346575657ec24ea252f24f4dde3f9b4fdb
Kamil Rataj <105726592+Kamilr616@users.noreply.github.com>
2024-12-06 12:34:50 +0100

    measurements migration hotfix

---
+++ src/server/SmartBotBlazorApp/Data/Migrations/20241206113341_measurements.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartBotBlazorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class measurements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RobotId",
+                table: "Measurements",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RobotId",
+                table: "Measurements",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
