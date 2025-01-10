5f50d9c11e33ea91842f2c39433b4b6a70f92fd1
Matix351 <130908742+Matix351@users.noreply.github.com>
2025-01-10 03:08:36 +0100

    JoystivckAndKeyboardUpdated
    Ale wm czy dobrze, bo juz mi sie mieni w oczach.... Powinno smigac

--- src/server/SmartBotBlazorApp/Components/RobotMovementInput/keyboardInputHandler.cs
+++ src/server/SmartBotBlazorApp/Components/RobotMovementInput/keyboardInputHandler.cs
@@ -14,6 +14,9 @@ namespace SmartBotBlazorApp.Components.RobotMovementInput
         public ROBOT_DIRECITON robotDir { get; private set; }
         public bool validInput { get; private set; }
 
+        public int leftEngine { get; private set; }
+        public int rightEngine { get; private set; }
+
         public keyboardInputHandler()
         {
             counter = 0;
@@ -22,6 +25,13 @@ namespace SmartBotBlazorApp.Components.RobotMovementInput
             validInput = false;
         }
 
+
+        public void onKeyUP(KeyboardEventArgs e)
+        {
+            keyName = "";
+            robotDir = ROBOT_DIRECITON.STOP;
+            validInput = false;
+        }
         public void onKeyDown(KeyboardEventArgs e)
         {
             counter++;
@@ -55,16 +65,54 @@ namespace SmartBotBlazorApp.Components.RobotMovementInput
 
                 default:
                     keyName = "Wrong Button";
-                    //robotDir = -1;
+                    robotDir = ROBOT_DIRECITON.STOP;
                     validInput = false;
                     break;
             }
+            translateKeysToEngineValues();
         }
 
         public void amIResetingCounter(bool reset)
         {
-            if(reset)
-                counter = 0;
+            if (!reset)
+                return;
+
+            counter = 0;
         }
+
+
+        //TODO zamienic to na slownik;
+        private void translateKeysToEngineValues()
+        {
+            switch (robotDir)
+            {
+                case ROBOT_DIRECITON.UP:
+                    leftEngine = 255;
+                    rightEngine = 255;
+                    break;
+                case ROBOT_DIRECITON.DOWN:
+                    leftEngine = -255;
+                    rightEngine = -255;
+                    break;
+                case ROBOT_DIRECITON.LEFT:
+                    leftEngine = 255;
+                    rightEngine = -255;
+                    break;
+                case ROBOT_DIRECITON.RIGHT:
+                    leftEngine = -255;
+                    rightEngine = 255;
+                    break;
+                case ROBOT_DIRECITON.STOP:
+                    leftEngine = 0;
+                    rightEngine = 0;
+                    break;
+            }
+        }
+
+        public (int, int) GetRobotEngineValues()
+        {
+            return (leftEngine, rightEngine);
+        }
+
     }
 }
